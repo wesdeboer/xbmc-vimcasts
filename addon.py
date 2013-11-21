7d3498497bfc046c0ac11d07bda21680ec81169b
Wes DeBoer <wesdeboer@gmail.com>
2013-11-20 21:45:38 -0800

    convert feed to string to replace invalid single quote escape before json parse to fix exception preventing it from loading after episode 59 was added

--- addon.py
+++ addon.py
@@ -26,7 +26,8 @@ def get_json_feed():
     '''Loads the JSON feed for vimcasts.org.'''
     json_url = 'http://vimcasts.org/episodes.json?referrer=xbmc'
     conn = urlopen(json_url)
-    _json = json.load(conn)
+    s = conn.read().replace("\\'","'")
+    _json = json.loads(s)
     conn.close()
     return _json
 
